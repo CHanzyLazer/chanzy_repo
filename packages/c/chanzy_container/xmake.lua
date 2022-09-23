7a622090bca60def818445a446b7050c88b2bc1e
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2022-09-23 15:17:08 +0800

    Update xmake.lua

--- packages/c/chanzy_container/xmake.lua
+++ packages/c/chanzy_container/xmake.lua
@@ -4,6 +4,7 @@ package("chanzy_container")
     set_license("MIT")
 
     add_urls("https://github.com/CHanzyLazer/chanzy_container.git")
+    add_versions("v1.0", "858075a44de1dd3fdd04f4e627259b41a0d9633e")
 
     -- install this handeronly package manually to avoid some unexpected bugs
     on_install(function (package)
